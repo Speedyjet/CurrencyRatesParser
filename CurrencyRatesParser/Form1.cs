4346b66b9ad5ab06c4f107813b6aa4ee5ce966f1
Speedyjet <Speedyjet@ua.fm>
2016-01-21 10:32:22 +0200

    Datapicker added
    Formatted datapicker added

--- CurrencyRatesParser/Form1.cs
+++ CurrencyRatesParser/Form1.cs
@@ -26,7 +26,8 @@ namespace CurrencyRatesParser
 
         private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
         {
-
+            dateTimePicker1.CustomFormat ="d.M.yyyy";
+            dateTimePicker1.Format = DateTimePickerFormat.Custom;
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -50,14 +51,13 @@ namespace CurrencyRatesParser
             ObjWorkSheet.Cells[2, 7] = "Покупка";
             ObjWorkSheet.Cells[2, 8] = "Продажа";
             var today = DateTime.Today;
-            var firstDay = DateTime.Parse("1.1.2014").Date;
+            var firstDay = DateTime.Parse(Convert.ToString(dateTimePicker1.Value)).Date;
             var j = 2;
             try
             {
                 for (var i = firstDay; i <= today; i = i.AddDays(1))
                 {
                     var client = new WebClient {Encoding = Encoding.UTF8};
-
                     var reply =
                         client.DownloadString(
                             "http://ru.kkb.kz/page/RatesConvertingOld?day=" + i.Day + "&month=" + i.Month + "&year=" +
@@ -69,22 +69,22 @@ namespace CurrencyRatesParser
                         nodes.Select(x => x.QuerySelectorAll("td").Select(y => y.InnerText).ToArray()).Skip(5).Take(23);
                     //.Skip(1)
                     //.Take(9);
-                    var application = new Application
-                    {
-                        DisplayAlerts = false
-                    };
+                    //var application = new Application
+                    //{
+                    //    DisplayAlerts = false
+                    //};
                     
                     foreach (var row in table.Where(row => row[0].StartsWith("1 ")))
                     {
                         j += 1;
                         ObjWorkSheet.Cells[j, 1] = i;
-                        ObjWorkSheet.Cells[j, 2] = row[0];
-                        ObjWorkSheet.Cells[j, 3] = row[1];
-                        ObjWorkSheet.Cells[j, 4] = row[2];
-                        ObjWorkSheet.Cells[j, 5] = row[3];
-                        ObjWorkSheet.Cells[j, 6] = row[4];
-                        ObjWorkSheet.Cells[j, 7] = row[5];
-                        ObjWorkSheet.Cells[j, 8] = row[6];
+                        ObjWorkSheet.Cells[j, 2] = row[0].ToString();
+                        ObjWorkSheet.Cells[j, 3] = row[1].ToString();
+                        ObjWorkSheet.Cells[j, 4] = row[2].ToString();
+                        ObjWorkSheet.Cells[j, 5] = row[3].ToString();
+                        ObjWorkSheet.Cells[j, 6] = row[4].ToString();
+                        ObjWorkSheet.Cells[j, 7] = row[5].ToString();
+                        ObjWorkSheet.Cells[j, 8] = row[6].ToString();
                     }
                 }
 
